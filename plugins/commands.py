2d1454bbf766767e49ca44c7bc1306a10208b735
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-13 16:53:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from Tools.Download import download
 
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "https://telegram.dog/BX_Botz"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
@@ -20,7 +20,7 @@ async def start(c, m):
     await c.send_message(chat_id=m.chat.id,
                          text=Translation.START.format(m.from_user.first_name, Config.USER_NAME),
                          reply_to_message_id=m.message_id,
-                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("My Father 👨‍💻", url=my_father), InlineKeyboardButton("📌Support channel", url=support)]]))
+                         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/BX_Botz"), InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/BX_Botz")],[InlineKeyboardButton("🤖 Bot Updates", url="https://t.me/BX_Botz"), InlineKeyboardButton("👥Support Group", url="https://t.me/BXSupport")]]))
     logger.info(f"{m.from_user.first_name} used start command")
 
 
